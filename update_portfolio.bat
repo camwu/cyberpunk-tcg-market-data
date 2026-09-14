eb89ed29eb637e828f7022d4de77bfae758f9d03
Cameron Wu <cameronwu2011@gmail.com>
2026-09-13 18:42:12 -0700

    fix(launcher): dispatch report via default OS file associations

--- update_portfolio.bat
+++ update_portfolio.bat
@@ -15,11 +15,4 @@ if exist "%~dp0.latest_report" set /p REPORT=<"%~dp0.latest_report"
 if not defined REPORT if exist "LATEST_PORTFOLIO_SUMMARY.md" set "REPORT=%CD%\LATEST_PORTFOLIO_SUMMARY.md"
 if not defined REPORT if exist "%~dp0LATEST_PORTFOLIO_SUMMARY.md" set "REPORT=%~dp0LATEST_PORTFOLIO_SUMMARY.md"
 
-if not defined REPORT goto :eof
-
-where code >nul 2>&1
-if %ERRORLEVEL% equ 0 (
-    call code -r "%REPORT%"
-) else (
-    start "" "%REPORT%"
-)
+if defined REPORT start "" "%REPORT%"
